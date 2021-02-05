5c35dbb9eb94688bad755d6632c4c62a753d1d51
Promiat <59731108+Promiat@users.noreply.github.com>
2021-02-05 17:30:33 +0100

    with books renderer

--- bibliotheque/app.py
+++ bibliotheque/app.py
@@ -1,5 +1,5 @@
 import sys, os
-from flask import Flask , render_template
+from flask import Flask , render_template, jsonify
 from markupsafe import escape
 
 app = Flask(__name__)
@@ -12,6 +12,20 @@ def index():
 @app.route("/votre_nom/<name>")
 def MyCustomName(name):
     return render_template('name.html', name=name)
+
+@app.route('/api/books', methods = ['GET'])
+def GetBook():
+    book=[
+        {
+            'id':1,
+            'titre' : 'un titre',
+        },
+        {
+            'id':2,
+            'titre': 'un autre titre random',
+        }
+    ]
+    return jsonify(book)
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0",debug=True, port=5000)
